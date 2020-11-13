9ba45677b538186d418fe73e9d4040afda893b9e
H2WO4 <arthurlamidel@gmail.com>
2020-11-13 16:57:01 +0100

    Suite 11 21

--- défis/crible_ératosthène.py
+++ défis/crible_ératosthène.py
@@ -2,4 +2,4 @@ liste = [*range(2, int(input("Entrez N : ")))]
 liste = [set([j for j in liste if (j % i != 0) or (j == i)]) for i in liste[0:len(liste)//2]]
 while len(liste) > 1:
     liste = [liste[i-1] & liste[i] for i in range(1, len(liste))]
-print(liste[0])
+print(sorted(list(liste[0])))
--- défis/suite.py
+++ défis/suite.py
@@ -1,3 +1,23 @@
-suite = "1"
-for i in suite:
-    
+suite, continuation = ["1"], 0
+for j in range(int(input("Quelle étape de la liste voulez vous voir : ")) - 1):
+    suite.append("")
+    for i in range(len(suite[j])):
+        if continuation > 0:
+            continuation -= 1
+            continue
+        try:
+            if suite[j][i] == suite[j][i+1] == suite[j][i+2]:
+                continuation = 2
+                suite[-1] += "3{}".format(suite[j][i])
+                continue
+        except IndexError:
+            pass
+        try:
+            if suite[j][i] == suite[j][i+1]:
+                continuation = 1
+                suite[-1] += "2{}".format(suite[j][i])
+                continue
+        except IndexError:
+            pass
+        suite[-1] += "1{}".format(suite[j][i])
+print(suite[-1])
