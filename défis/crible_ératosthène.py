597f6d892a8f99131c5413980563c68a76a2ca9f
H2WO4 <54752489+H2WO4@users.noreply.github.com>
2020-11-17 10:52:27 +0100

    a

--- défis/crible_ératosthène.py
+++ défis/crible_ératosthène.py
@@ -1,5 +1,5 @@
-liste = [*range(2, int(input("Entrez N : ")))]
-liste = [set([j for j in liste if (j % i != 0) or (j == i)]) for i in liste[0:len(liste)//2]]
+liste = [*range(2, int(input("Entrez N : "))+1)]
+liste = [set([j for j in liste if (j % i != 0) or (j == i)]) for i in liste[0:int(len(liste)**0.5)+1]]
 while len(liste) > 1:
-    liste = [liste[i-1] & liste[i] for i in range(1, len(liste))]
+    liste = [liste[i-1] & liste[i] for i in range(1, len(liste), 2)]
 print(sorted(list(liste[0])))
--- td4/listePremiers.py
+++ td4/listePremiers.py
@@ -1,1 +1,1 @@
-[print(i) for i in range(2, int(input("Valeur de n : "))+1) if len([j for j in range(1, i//2 + 1) if i % j == 0]) == 1]
+print([i for i in range(2, int(input("Valeur de n : "))+1) if len([j for j in range(1, int(i**0.5)+1) if i % j == 0]) == 1])
